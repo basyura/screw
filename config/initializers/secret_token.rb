712d200bd235fb6af84d059c0903fe02ad458e07
basyura <basyura@gmail.com>
2011-06-11 22:09:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Typhoo::Application.config.secret_token = 'c05781007d78316549541f41aaf4ff0b67a25a9c51e3126e43a1854e298b3fc5219d99503ac4872b30023b9e26de4a0ce66c16c527ed25dfaaa516ed2bea4193'
