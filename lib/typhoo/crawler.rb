1496f131ae8907adc30beea0d237e664c2c7f4dd
basyura <basyura@gmail.com>
2011-06-20 16:15:42 +0900

    proxy support

--- lib/typhoo/crawler.rb
+++ lib/typhoo/crawler.rb
@@ -9,6 +9,24 @@ require 'pstore'
 require 'uri'
 require 'yaml'
 
+=begin
+
+wedata api
+
+{
+  "name"=>"タイトル", 
+  "resource_url"=>"http://wedata.net/items/54438", 
+  "updated_at"=>"2011-06-13T20:21:16+09:00", 
+  "created_by"=>"cabkip", 
+  "database_resource_url"=>"http://wedata.net/databases/LDRFullFeed", 
+  "data"=>{
+    "url"=>"^http://hogehoge\\.net/", 
+    "type"=>"IND", 
+    "xpath"=>"//div[@class=\"entry-body clearfix\"]"}, 
+    "created_at"=>"2011-06-13T20:07:32+09:00"
+  }
+=end
+
 module Typhoo
   class Crawler
     include ActionView::Helpers::SanitizeHelper
@@ -35,7 +53,12 @@ module Typhoo
       end
       xpath = xpath.gsub(/(id\("(.*?)"\))/,'[@id="\\2"]')
       puts "xpath = #{xpath}"
-      page = Mechanize.new.get(url)
+      agent = Mechanize.new
+      if ENV['http_proxy']
+        proxy = URI.parse(ENV['http_proxy'])
+        agent.set_proxy(proxy.host , proxy.port)
+      end
+      page  = agent.get(url)
       #page.encoding = encoding if encoding
       while true
         begin
@@ -92,56 +115,7 @@ module Typhoo
 end
 
 if __FILE__ == $0
-  #url = 'http://d.hatena.ne.jp/basyura'
-  #url = 'http://blog.livedoor.jp/nwknews/archives/3803948.html'
   url = 'http://news.2chblog.jp/archives/51614888.html'
   p Typhoo::Crawler.new.crawl(url)
 end
-
-#url = 'http://blog.livedoor.jp/nwknews/archives/3803948.html'
-#info = Typhoo::Crawler.new.crawl(url)
-#puts info[:content]
-
-=begin
-{
-  "name"=>"タイトル", 
-  "resource_url"=>"http://wedata.net/items/54438", 
-  "updated_at"=>"2011-06-13T20:21:16+09:00", 
-  "created_by"=>"cabkip", 
-  "database_resource_url"=>"http://wedata.net/databases/LDRFullFeed", 
-  "data"=>{
-    "url"=>"^http://hogehoge\\.net/", 
-    "type"=>"IND", 
-    "xpath"=>"//div[@class=\"entry-body clearfix\"]"}, 
-    "created_at"=>"2011-06-13T20:07:32+09:00"
-  }
-=end
-
-
-
-
-#target = 'http://d.hatena.ne.jp/basyura'
-#target = 'http://headlines.yahoo.co.jp/hl?a=20110616-00000131-jij-soci'
-#target = 'http://vinarian.blogspot.com/2010/10/saint-vimmer.html'
-#target = 'http://qwik.jp/guruby/21.html'
-#target = 'http://alfalfalfa.com/archives/3576605.html'
-#target = 'http://blog.livedoor.jp/dankogai/archives/51695967.html'
-#target = 'http://news.livedoor.com/article/detail/5637705/'
-
-#crawl('http://saxbluemurmur.jugem.cc/?cid=5' , '//div[(@class="entry_body") or (@class="entry_more")]' , 'saxbluemurmur' , 'euc-jp')
-#crawl('http://acru.jp/blog/staff_blog/1355' , 'div[contains(concat(" ", @class, " "), " details ")]d' , 'aru')
-#crawl('http://www.theatlantic.com/infocus/2011/06/diy-weapons-of-the-libyan-rebels/100086/' , '//div[@class="articleContent"]|//div[@class="articleContent"]/following-sibling::span' , 'infocus')
-#crawl('http://d.hatena.ne.jp/basyura/20110502' , 'id("days")//div[@class="body"]//h3/following-sibling::*' , 'basyura.hatena')
-#crawl('http://d.hatena.ne.jp/basyura/20110502' , 'id("days")//div[@class="body"]' , 'basyura.hatena')
-#crawl('http://blog.livedoor.jp/booq/archives/1444153.html' , 'id("blogbody_id")' , 'livedoor')
-#crawl('http://www.sonypictures.jp/blogs/sonypicturesplus/2011/06/post_371.php' , '//div[@class="content"]' , 'sonypictures')
-#crawl('http://d.hatena.ne.jp/basyura/' , 'id("days")//div[@class="body"]' , 'basyura.hatena')
-#crawl('http://blog.livedoor.jp/booq/'  , 'id("blogbody_id")' , 'livedoor')
-#crawl('http://www.sonypictures.jp/blogs/sonypicturesplus/' , '//div[@class="content"]' , 'sonypictures')
-#crawl('http://blog.livedoor.jp/dankogai/archives/51694301.html' , '//div[@class="main"]' , 'dankogai')
-#crawl('http://blog.livedoor.jp/dankogai/' , '//div[@class="main"]' , 'dankogai')
-#crawl('http://www.47news.jp/localnews/hotnews/2011/06/post-20110615161911.html' , 'id("bt_body")' , 'localnews')
-#crawl('http://www.47news.jp/localnews/' , 'id("bt_body")' , 'localnews')
-#
-#
 
